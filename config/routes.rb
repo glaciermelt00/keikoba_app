1387f96f43d8216b9a8a76bcb1853e7ad9a64fc1
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-05-05 22:48:54 +0900

    Create User model & Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root 'application#hello'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'users/destroy'
+  resources :users
+  get 'users/guest'
+  get 'users/following'
+  get 'users/followers'
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /show" do
+    it "returns http success" do
+      get "/users/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/users/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/users/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/users/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/users/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/users/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /guest" do
+    it "returns http success" do
+      get "/users/guest"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /following" do
+    it "returns http success" do
+      get "/users/following"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /followers" do
+    it "returns http success" do
+      get "/users/followers"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
