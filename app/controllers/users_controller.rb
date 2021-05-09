13d2b69f978ba94b0d7f17cc3655bb61a813f316
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-05-09 23:06:14 +0900

    feat: style: Signup 機能を作成し、合わせてSignupページおよびUser Topページのスタイルを調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "KEIKOBAへようこそ！"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
   def edit; end
 
@@ -20,4 +30,10 @@ class UsersController < ApplicationController
   def following; end
 
   def followers; end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
