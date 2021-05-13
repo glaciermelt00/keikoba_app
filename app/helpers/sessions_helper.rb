a30c384c24733879b85d7b07e5060b7746f42d18
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-05-13 22:49:28 +0900

    fix: シンボルを修正し、フレンドリーフォワーディングを実現

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -50,7 +50,7 @@ module SessionsHelper
 
   # 記憶したURL（もしくはデフォルト値）にリダイレクト
   def redirect_back_or(default)
-    redirect_to(session[:forwaiding_url] || default)
+    redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
   end
 
