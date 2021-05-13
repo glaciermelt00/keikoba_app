33be32a8fb8220e4f9fba7a5070cd0fdd1d236e9
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-05-13 21:47:59 +0900

    feat: 管理者権限を付与した

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_011950) do
+ActiveRecord::Schema.define(version: 2021_05_13_123339) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.string "remember_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.boolean "guest"
     t.string "activation_digest"
     t.boolean "activated"
