39b01ab3e2337552e8373784306ee081a1bab940
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-05-22 09:54:13 +0900

    fix: Userのnameのlength範囲を3..50に広げた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   before_save :downcase_email
   before_create :create_activation_digest
-  validates :name, presence: true, length: { in: 4..50 }
+  validates :name, presence: true, length: { in: 3..50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
