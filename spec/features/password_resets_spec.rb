9e1c58991a2ff5a7f868a524e9ebe38e3f30667a
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-06-06 12:02:39 +0900

    test: refactor: feature spec完成。一部リファクタリング。

--- spec/features/password_resets_spec.rb
+++ spec/features/password_resets_spec.rb
@@ -35,5 +35,58 @@ RSpec.feature "PasswordResets", type: :feature do
 
     # activateされていないユーザーでは、rootにリダイレクトされる
     user.toggle!(:activated)
+    visit edit_password_reset_path(reset_token, email: user.email)
+    expect(current_path).to eq root_path
+    user.toggle!(:activated)
+
+    # 無効なトークンでは、rootにリダイレクトされる
+    visit edit_password_reset_path('wrong token', email: user.email)
+    expect(current_path).to eq root_path
+
+    # 有効なトークンとemaildで、再設定ページにアクセスできる
+    visit edit_password_reset_path(reset_token, email: user.email)
+    expect(current_path).to eq edit_password_reset_path(reset_token)
+    expect(page).to have_selector 'h2.text-center', text: '新しいパスワードを設定'
+    expect(find('#email', visible: false).value).to eq user.email
+
+    # 無効なパスワードとパスワード再入力では、updateできない
+    fill_in '新しいパスワード',	with: 'foobaz'
+    fill_in '新しいパスワードの再入力',	with: 'barquux'
+    click_button '送信'
+    expect(page).to have_selector '#error_explanation'
+
+    # パスワードが空では、updateできない
+    fill_in '新しいパスワード',	with: ''
+    fill_in '新しいパスワードの再入力',	with: ''
+    click_button '送信'
+    expect(page).to have_selector '#error_explanation'
+
+    # 有効なパスワードとパスワード確認で、ログインでき、reset_digestがからにな利、userページにリダイレクトされる
+    fill_in '新しいパスワード',	with: 'foobaz'
+    fill_in '新しいパスワードの再入力',	with: 'foobaz'
+    click_button '送信'
+    expect(page).to have_content 'ログアウト'
+    expect(current_path).to eq user_path(user)
+    expect(user.reload.reset_digest).to be_nil
+  end
+
+  # トークンの有効期限が切れると、パスワードを再設定できない
+  scenario 'cannot reset password after the expiration date of reset token' do
+    user = create(:user, :do_activate)
+
+    visit root_path
+    click_link 'ログイン'
+    click_link 'パスワードを忘れた方'
+    fill_in 'メールアドレス',	with: user.email
+    click_button '送信'
+
+    reset_email = ActionMailer::Base.deliveries.last
+    email_body = reset_email.html_part.body.encoded
+    reset_token = email_body[/(?<=password_resets\/)[^\/]+/]
+
+    user.update_attribute(:reset_sent_at, 3.hours.ago)
+    visit edit_password_reset_path(reset_token, email: user.email)
+    save_and_open_page
+    expect(page).to have_selector 'div.alert-danger', text: '有効期限が切れているため、パスワードを再設定できませんでした'
   end
 end
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -38,4 +38,84 @@ RSpec.feature "Posts", type: :feature do
 
     expect(page.driver.browser.switch_to.alert.text).to eq '画像のサイズが5MBを超えています。5MB未満の画像を選択して下さい。'
   end
+
+  scenario 'post interface', js: true do
+    user = create(:user, :do_activate)
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード',	with: user.password
+    click_button 'ログイン'
+    find(".fa-edit").click
+    expect(page).to have_selector 'h2', text: '新規投稿'
+
+    # 無効なpostは作成されない
+    expect {
+      fill_in '名称', with: ''
+      click_button '投稿する'
+    }.to_not change(user.posts, :count)
+    expect(page).to have_selector 'div.alert-danger', text: 'エラー発生： 1個'
+
+    # 有効なpostは作成される
+    expect {
+      fill_in '名称', with: 'Test Post'
+      attach_file '画像アップロード', "#{Rails.root}/spec/fixtures/images/cat_5kb.jpg"
+      click_button '投稿する'
+    }.to change(user.posts, :count).by(1)
+    expect(page).to have_selector 'div.alert-success', text: '投稿できました！'
+    post_name = user.posts.last.name
+    expect(page).to have_selector 'a.card-title', text: post_name
+    expect(page).to have_selector "img[src$='cat_5kb.jpg']"
+
+    # 投稿を削除できる
+    click_link user.posts.last.name
+    expect(page).to have_selector 'a.btn-danger', text: '削除する'
+    expect {
+      click_link '削除する'
+      expect(page.accept_confirm).to eq "削除しますか？"
+      expect(page).to have_selector 'div.alert-success', text: '投稿が削除されました'
+    }.to change(user.posts, :count).by(-1)
+
+    expect(current_path).to eq user_path(user)
+    expect(page).to_not have_selector 'a', text: post_name
+
+    # 他のユーザーのpostは削除できない
+    other_user = create(:user, :do_activate)
+    create_list(:post, 3, user: other_user)
+
+    find(".fa-users").click
+    click_link other_user.name
+    click_link other_user.posts.last.name
+    expect(current_path).to eq post_path(other_user.posts.last)
+    expect(page).to_not have_content '削除する'
+  end
+
+  # 投稿数をカウントできる
+  scenario 'can count posts' do
+    user = create(:user, :do_activate)
+    create_list(:post, 50, user: user)
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード',	with: user.password
+    click_button 'ログイン'
+
+    expect(page).to have_selector 'h3', text: "投稿 (#{user.posts.count}件)"
+    click_link 'ログアウト'
+
+    # postしていない場合は0件、postしたら1件が表示される
+    other_user = create(:user, :do_activate)
+
+    click_link 'ログイン'
+    fill_in 'メールアドレス', with: other_user.email
+    fill_in 'パスワード',	with: other_user.password
+    click_button 'ログイン'
+    expect(page).to have_selector 'h3', text: "投稿 (0件)"
+
+    create(:post, user: other_user)
+    visit current_path
+    expect(page).to have_selector 'h3', text: "投稿 (1件)"
+  end
 end
