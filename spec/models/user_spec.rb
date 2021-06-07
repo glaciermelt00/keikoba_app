7c5f5ba5f6f7140829c59d76cfd02b00521a88ee
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-06-07 21:17:37 +0900

    test: refactor: Shoulda Matchersでリファクタリング.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:user) { build(:user, password: '', password_confirmation: '') }
+  subject(:user) { build(:user) }
+
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_length_of(:name).is_at_least(3).is_at_most(50) }
   it { is_expected.to validate_presence_of :email }
+
+  valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+  valid_addresses.each do |valid_address|
+    it { is_expected.to allow_value(valid_address).for(:email) } 
+  end
+
+  invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+  invalid_addresses.each do |invalid_address|
+    it { is_expected.to_not allow_value(invalid_address).for(:email) } 
+  end
+
   it { is_expected.to validate_length_of(:email).is_at_most(255) }
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { is_expected.to have_secure_password :password }
-  it { is_expected.to validate_presence_of :password }
+  it { is_expected.to validate_presence_of(:password).allow_nil }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
   it { is_expected.to have_many(:posts).dependent(:destroy) }
   it { is_expected.to have_many(:favorites).dependent(:destroy) }
@@ -18,38 +30,14 @@ RSpec.describe User, type: :model do
 
   # name, email, passwordがあれば、有効な状態であること
   it 'is valid with a name, email, and password' do
-    @user = build(:user)
-    expect(@user).to be_valid
+    expect(user).to be_valid
   end
 
-  describe 'email' do
-    # フォーマットに合致するemailなら、有効な状態であること
-    it 'is valid with an email that matches the format ' do
-      @user = build(:user)
-      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
-      valid_addresses.each do |valid_address|
-        @user.email = valid_address
-        expect(@user).to be_valid
-      end
-    end
-
-    # フォーマットに合致しないemailなら、無効な状態であること
-    it 'is invalid with an email that does not match the format' do
-      @user = build(:user)
-      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
-      invalid_addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.valid?
-        expect(@user.errors[:email]).to include('は不正な値です')
-      end
-    end
-
-    # DBに保存されたemailは小文字であること
-    it 'makes email downcase when user is saved to DB' do
-      mixed_case_email = 'Foo@ExAMPle.CoM'
-      @user = create(:user, email: mixed_case_email)
-      expect(mixed_case_email.downcase).to eq @user.email
-    end
+  # DBに保存されたemailは小文字であること
+  it 'makes email downcase when user is saved to DB' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    user.update_attribute(:email, mixed_case_email)
+    expect(mixed_case_email.downcase).to eq user.email
   end
 
   # rememberメソッドを呼び出すと、remember_digestが上書きされる
@@ -66,8 +54,7 @@ RSpec.describe User, type: :model do
 
   # digestがnilなら、authenticated?メソッドはfalseを返す
   it 'returns false with authenticated? method if digest is nil' do
-    @user = build(:user)
-    expect(@user.authenticated?(:remember, '')).to be false
+    expect(user.authenticated?(:remember, '')).to be false
   end
 
   # forgetメソッドで、remember_digestがnilになる
@@ -91,39 +78,37 @@ RSpec.describe User, type: :model do
 
   # create_reset_digestメソッドで、reset_digestが上書きされる
   it 'updates reset_digest with create_reset_digest method' do
-    @user = create(:user)
-    @user.create_reset_digest
-    expect(@user.reset_digest).to_not be_nil
+    user.save
+    user.create_reset_digest
+    expect(user.reset_digest).to_not be_nil
   end
 
   # create_reset_digestメソッドで、reset_sent_atが上書きされる
   it 'updates reset_sent_at with create_reset_digest method' do
-    @user = create(:user)
-    @user.create_reset_digest
-    expect(@user.reset_sent_at).to_not be_nil
+    user.save
+    user.create_reset_digest
+    expect(user.reset_sent_at).to_not be_nil
   end
 
   # reset_sent_atから2時間過ぎたら、password_reset_expired?メソッドはtrueを返す
   it 'returns true with password_reset_expired? method if it takes 2 hours from reset_sent_at' do
-    @user = create(:user)
-    @user.create_reset_digest
-    @user.reset_sent_at = 2.hours.ago
-    expect(@user.password_reset_expired?).to be true
+    user.save
+    user.create_reset_digest
+    user.reset_sent_at = 2.hours.ago
+    expect(user.password_reset_expired?).to be true
   end
 
   # before_createで発生するprivateのcreate_activation_digestで、activation_tokenが作成される
   it 'creates activation_token with privated create_activation_digest method caused by before_create' do
-    @user = build(:user)
-    expect(@user.activation_token).to be_nil
-    @user.save
-    expect(@user.activation_token).to_not be_nil
+    expect(user.activation_token).to be_nil
+    user.save
+    expect(user.activation_token).to_not be_nil
   end
 
   # before_createで発生するprivateのcreate_activation_digestで、activation_digestが作成される
   it 'creates activation_digest with privated create_activation_digest method caused by before_create' do
-    @user = build(:user)
-    expect(@user.activation_digest).to be_nil
-    @user.save
-    expect(@user.activation_digest).to_not be_nil
+    expect(user.activation_digest).to be_nil
+    user.save
+    expect(user.activation_digest).to_not be_nil
   end
 end
