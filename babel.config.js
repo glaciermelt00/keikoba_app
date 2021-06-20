9288944a5b40e6291fa81aa71d82e1e46dafff47
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-06-20 15:14:13 +0900

    fix: babelにloose:trueを追加。bundle updateでGemfile.lockを更新。

--- babel.config.js
+++ babel.config.js
@@ -66,7 +66,13 @@ module.exports = function(api) {
         {
           async: false
         }
-      ]
+      ],
+      [
+        "@babel/plugin-proposal-private-methods",
+        {
+          "loose": true
+        }
+      ] 
     ].filter(Boolean)
   }
 }
