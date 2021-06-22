8b7574a3d0c32af880d80351ec1379eb0d14642d
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-06-22 22:34:17 +0900

    fix: 配列の書き方を変更。

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -66,8 +66,8 @@ server '35.74.35.169',
   ssh_options: {
     # port: 22022,
     # user: "odatakashi", # overrides user setting above
-    auth_methods: ['publickey'],
-    keys: ['~/.ssh/myserverkey.pem']
+    auth_methods: %w(publickey),
+    keys: %w(~/.ssh/myserverkey.pem)
     forward_agent: true
     #     # password: "please use keys"
   }
