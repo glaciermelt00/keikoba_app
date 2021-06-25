2b9ed373f6bd68a11d9ea9244e173aea4ba4ff6d
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-06-26 00:18:03 +0900

    fix; smtp_settingsを、config.action_mailerに変更した。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { host: 'ec2-35-74-35-169.ap-northeast-1.compute.amazonaws.com' }
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     # address のところは変更しないので注意。自分はここでハマりました。
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
